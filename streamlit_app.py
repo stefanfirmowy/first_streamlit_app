f8c9694c4a93f4fdde0ab29ff6f088e88044127f
Stefan <107257305+stefanfirmowy@users.noreply.github.com>
2022-06-10 15:14:32 +0200

    Update streamlit_app.py
    add graphics

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Brekfast Menu')
-streamlit.text('Omega 3 & Bluberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('🥣 Brekfast Menu')
+streamlit.text('🥗  Omega 3 & Bluberry Oatmeal')
+streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
